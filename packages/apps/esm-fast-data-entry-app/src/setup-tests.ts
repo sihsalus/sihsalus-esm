3845ec24076aeaf805f26b536a25cdd38f258298
Gonzalo <gonzalo.galvezc@pucp.edu.pe>
2026-04-13 18:39:41 -0500

    fix: mejorar la configuración de Jest y agregar soporte para ResizeObserver en pruebas

--- packages/apps/esm-fast-data-entry-app/src/setup-tests.ts
+++ packages/apps/esm-fast-data-entry-app/src/setup-tests.ts
@@ -1,1 +1,11 @@
 import '@testing-library/jest-dom';
+
+class ResizeObserver {
+	observe() {}
+	unobserve() {}
+	disconnect() {}
+}
+
+if (!global.ResizeObserver) {
+	(global as any).ResizeObserver = ResizeObserver;
+}
