d6fc990fe29b5004805e0977288163d721249559
Duvet05 <gonzalo.galvezc@pucp.edu.pe>
2026-05-05 09:59:44 -0500

    fix: eliminar importaciones innecesarias de React en varios componentes

--- packages/apps/esm-help-menu-app/src/help-menu/help-popup.component.tsx
+++ packages/apps/esm-help-menu-app/src/help-menu/help-popup.component.tsx
@@ -1,5 +1,4 @@
 import { ExtensionSlot } from '@openmrs/esm-framework';
-import React from 'react';
 import { useTranslation } from 'react-i18next';
 
 import styles from './help-popup.styles.scss';
